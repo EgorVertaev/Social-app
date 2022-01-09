efc077149dd03cc33f61f6e21cc50f05246adfdd
EgorVertaev <eholife123@gmail.com>
2022-01-09 15:51:42 +0200

    add MyPosts and Post refactoring styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./style/App.css";
 import "./style/reset.css";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
